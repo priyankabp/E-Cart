d4f15b78269090edda82e44d97ebefafb6eb6c4d
Priyanka Phapale <pbphapale@gmail.com>
2018-04-06 18:21:54 -0400

    Add product model

---
+++ models/product.js
@@ -0,0 +1,14 @@
+import { model } from 'mongoose';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+    imagePath: {type:String, required: true},
+    title: {type:String, required: true},
+    description: {type:String, required: true},
+    price: {type:Number, required: true},
+
+});
+
+model.exports = mongoose.model('Product',schema);
